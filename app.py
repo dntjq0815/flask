8c02682f688d2ab8cd5c43e399a5689dc10bac32
dntjq0815 <dntjq0815@naver.com>
2023-09-14 10:15:45 +0900

    add requirement.txt

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import sys
+sys.path.append('c:\\users\\sim\\anaconda3\\lib\\site-packages')
+
 app = Flask(__name__)
- 
+
 @app.route("/")
 def home():
     return "Hello, Flask"
